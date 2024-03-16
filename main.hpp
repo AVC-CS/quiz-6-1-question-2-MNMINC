fc7244e29e7f59631f4248b95a3aae99c8eced0e
MNMINC <157410433+MNMINC@users.noreply.github.com>
2024-03-16 03:05:31 +0000

    cout trout

--- main.hpp
+++ main.hpp
@@ -40,7 +40,7 @@ int readFile(string fileName)
         exit(0);
     }
 
-    int N, total = 0;
+    int N;
 	ifs >> N; 
 	for (int i = 0; i < N; ++i)
 	{
@@ -49,9 +49,7 @@ int readFile(string fileName)
         double salery;
 		ifs >> id >> name >> job >> salery;
 		cout << id << " " << name << " " << job << " " << salery << endl;
-        total += salery;
 	}
-    cout << "Total: " << total << " Average: " << total / N << endl;
 
     return N;
 }
